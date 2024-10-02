8ca3adae634a1390ffd02119f397615f1ff78535
Andres Marquez <andresdmf55@gmail.com>
2024-10-01 23:12:09 -0500

    Add internalization i18n base configuration, with 2 languages. Also, we add implementation in nav links

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+    en: 'English',
+    es: 'Español',
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    en: {
+      'nav.home': 'Home',
+      'nav.experience': 'Experience',
+      'nav.projects': 'Projects',
+    },
+    es: {
+      'nav.home': 'Inicio',
+      'nav.experience': 'Experiencia',
+      'nav.projects': 'Proyectos',
+    },
+  } as const;
