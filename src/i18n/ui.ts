72afd8bd7443f82e1e0d2e40b5d152266a935084
Andres Marquez <andresdmf55@gmail.com>
2024-10-02 22:05:40 -0500

    Modify internalization structure and ui data

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,19 +1,33 @@
 export const languages = {
-    en: 'English',
-    es: 'Español',
+    en: "English",
+    es: "Español",
   };
   
-  export const defaultLang = 'en';
+  export const defaultLang = "en";
   
   export const ui = {
     en: {
-      'nav.home': 'Home',
-      'nav.experience': 'Experience',
-      'nav.projects': 'Projects',
+      "nav.home": "Home",
+      "nav.experience": "Experience",
+      "nav.projects": "Projects",
+      "profile.gretting": "Hey, I'm Andres",
+      "profile.description-1": "FullStack Software Developer.",
+      "profile.description-2": "Creating user interfaces, improving UI and UX with",
+      "profile.description-3": " best practices.",
+      "profile.tag": "Open to work",
+      "profile.cv": "Open CV",
+      "profile.email": "andresdmf55@gmail.com",
     },
     es: {
-      'nav.home': 'Inicio',
-      'nav.experience': 'Experiencia',
-      'nav.projects': 'Proyectos',
+      "nav.home": "Inicio",
+      "nav.experience": "Experiencia",
+      "nav.projects": "Proyectos",
+      "profile.gretting": "Hola, soy Andrés",
+      "profile.description-1": "FullStack Software Developer.",
+      "profile.description-2": "part 1",
+      "profile.description-3": " part 2",
+      "profile.tag": "Disponible para trabajar",
+      "profile.cv": "Abrir CV",
+      "profile.email": "andresdmf55@gmail.com",
     },
   } as const;
