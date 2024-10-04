febc98f9179b2889982f4db60ab51ed9097be5a1
Andres Marquez <andresdmf55@gmail.com>
2024-10-03 21:20:46 -0500

    Modify data and create json file for that. Also, donwload a boxicon dependency and add open cv icon

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,3 +1,5 @@
+import data from "./data.json";
+
 export const languages = {
     en: "English",
     es: "Español",
@@ -5,33 +7,4 @@ export const languages = {
   
   export const defaultLang = "en";
   
-  export const ui = {
-    en: {
-      "nav.home": "Home",
-      "nav.experience": "Experience",
-      "nav.projects": "Projects",
-      "profile.gretting": "Hey, I'm Andres",
-      "profile.description-1": "FullStack Software Developer.",
-      "profile.description-2": "Creating user interfaces, improving UI and UX with",
-      "profile.description-3": " best practices.",
-      "profile.tag": "Open to work",
-      "profile.cv": "Open CV",
-      "profile.email": "andresdmf55@gmail.com",
-      "profile.icon.linkedin": 'https://www.linkedin.com/in/andresmarquezdev/',
-      "profile.icon.github": 'https://github.com/andresdmdev',
-    },
-    es: {
-      "nav.home": "Inicio",
-      "nav.experience": "Experiencia",
-      "nav.projects": "Proyectos",
-      "profile.gretting": "Hola, soy Andrés",
-      "profile.description-1": "FullStack Software Developer.",
-      "profile.description-2": "part 1",
-      "profile.description-3": " part 2",
-      "profile.tag": "Disponible para trabajar",
-      "profile.cv": "Abrir CV",
-      "profile.email": "andresdmf55@gmail.com",
-      "profile.icon.linkedin": 'https://www.linkedin.com/in/andresmarquezdev/',
-      "profile.icon.github": 'https://github.com/andresdmdev',
-    },
-  } as const;
+  export const ui = data;
