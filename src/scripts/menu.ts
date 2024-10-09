1fbd81b28eaba150b4c3839f7c45f8cd216ace95
Andres Marquez <andresdmf55@gmail.com>
2024-10-09 06:49:01 -0500

    Modify menu styles and logic

--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -2,3 +2,9 @@ document.querySelector('.hamburger')?.addEventListener('click', () => {
     document.querySelector('.nav-links')?.classList.toggle('expanded');
     document.querySelector('.hamburger')?.classList.toggle('hamburger-toogle');
 });
+
+document.querySelectorAll('.nav-link').forEach(link => link.addEventListener('click', () => {
+        document.querySelector('.nav-links')?.classList.toggle('expanded');
+        document.querySelector('.hamburger')?.classList.toggle('hamburger-toogle');
+    })
+);
