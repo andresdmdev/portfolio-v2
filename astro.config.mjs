b130dbb01aca45a5cff2b0185c64783b310cae76
Andres Marquez <andresdmf55@gmail.com>
2024-10-13 13:13:18 -0500

    Remove unnecessary depencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import preact from '@astrojs/preact';
-
 // https://astro.build/config
-export default defineConfig({
-  integrations: [preact({ compat: true })],
-});
+export default defineConfig({});
