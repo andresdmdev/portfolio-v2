e4b7663b8d9e10fb253c5709e61dd55ec24e869b
Andres Marquez <andresdmf55@gmail.com>
2025-07-23 06:56:25 -0500

    Refactor translation access in Education, Experience, and Stack components; enhance Carrousel interface and update styles for improved layout and responsiveness

--- src/interfaces/projectItem.ts
+++ src/interfaces/projectItem.ts
@@ -1,5 +1,25 @@
 export interface ProjectItem {
     title: string,
     description: string,
-    className: string
+    className: string,
+    technologies: string[],
+    images: Image[],
+    links: Link[]
+}
+
+export interface Link {
+    name: string;
+    url: string;
+    icon: string;
+}
+
+export interface Image {
+    image: string;
+    altText: string;
+}
+
+export interface Technology {
+    technology: string;
+    backgroundColor: string;
+    logo: string;
 }
